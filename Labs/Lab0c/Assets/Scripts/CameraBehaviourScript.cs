cf83896bef31cd131f5382e5825e14824e94f3cb
FahadAKhan24 <fahad24@my.yorku.ca>
2020-02-28 22:48:57 -0500

    Lab 0c submission

---
+++ Labs/Lab0c/Assets/Scripts/CameraBehaviourScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviourScript : MonoBehaviour 
+
+   
+{
+
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	  //if (Input.GetKeyDown(KeyCode.T))
+	 // {
+        // TRANSLATION with respect to Y axis
+		
+		 if (Input.GetKeyDown(KeyCode.W)) // UP
+        {
+            transform.Translate(0, 3 , 0);  
+        }
+		else if (Input.GetKeyDown(KeyCode.D)) // RIGHT
+		{
+			transform.Translate(3, 0 , 0);  
+		}
+		else if (Input.GetKeyDown(KeyCode.X)) // DOWN
+		{
+			transform.Translate(0, -3 , 0);  
+		}
+		else if (Input.GetKeyDown(KeyCode.A)) // LEFT
+		{
+			transform.Translate(-3, 0 , 0);  
+		}
+		else
+        {
+			// do nothing
+		}	
+      //}
+	  
+      //else if (Input.GetKeyDown(KeyCode.R)) 
+	  //{
+         // ROTATION about Z axis
+		 
+		 if (Input.GetKeyDown(KeyCode.I)) // +90
+        {
+            transform.Rotate( 0.0f, 0.0f , 90.0f);  
+        }
+		else if (Input.GetKeyDown(KeyCode.P)) // +180
+		{
+			transform.Rotate( 0.0f, 0.0f , 180.0f);  
+		}
+		else if (Input.GetKeyDown(KeyCode.M)) // -90
+		{
+			transform.Rotate( 0.0f, 0.0f , -90.0f);  
+		}
+		else if (Input.GetKeyDown(KeyCode.J)) // -180
+		{
+			transform.Rotate( 0.0f, 0.0f , -180.0f);  
+		}
+		else
+        {
+			// do nothing
+		}
+        //}
+
+        //else 
+        // {
+        // do nothing
+        // }		  
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("fly");
+        }
+        
+    }
+
+    
+}
+
+
+// transform.Translate(0, 0, Time.deltaTime);
+// transform.Rotate(90.0f, 0.0f, 0.0f);
---
+++ Labs/Lab0c/Assets/Scripts/GameObjectBehaviourScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectBehaviourScript : MonoBehaviour  // this is to be attached to CUBE
+{
+
+    // public GameObject Cube;
+	// public GameObject Plane;
+   // public GameObject PhotoPlane;
+   // HOW DO I REFER TO GAMEOBJECT CUBE ITSELF? 
+   // SHOULD THERE BE TWO DIFFERENT SCRIPTS FOR CUBE AND SPHERE INORDER TO SWITCH BETWEEN THEM
+   
+    public GameObject Sphere;
+	// You can now drag an object from the scene
+    // or Hierarchy panel onto this variable to assign it in inspector window
+	
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Cube = GameObject.Find("Cube");
+        Sphere = GameObject.Find("Sphere");
+        Sphere.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (gameObject.activeSelf == true)             // cube is active so switch to Sphere
+            {
+                //gameObject.SetActive(false);
+				//gameObject.SetActive(false);
+                Sphere.SetActive(true);
+                Sphere.transform.position = new Vector3(2, 4, 0);
+                
+
+               
+            }
+
+            else if (Sphere.activeSelf == true)    // sphere is active so switch to Cube
+            {
+               // Sphere.SetActive(false);
+                gameObject.SetActive(true);
+                //gameObject.SetActive(true);
+                gameObject.transform.position = new Vector3(0, 4, 0);
+				//gameObject.transform.position = new Vector3(0, 3.5, 0);
+            }
+
+            else {
+                  // do nothing
+            }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+
+            Destroy(Sphere);  // currently selected game object
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+
+            Instantiate(Sphere, new Vector3(3, 1, 0), Quaternion.identity);  // currently selected game object
+
+        }
+        
+    }
+}
